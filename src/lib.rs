75f22d47e7d9d12d40c823cfbc6bb2afaa1a732d
Benedikt Grande <dev@bgrande.de>
2022-12-16 13:19:52 +0100

    feat(2pdf): html2pdf integration
    this is a test and it might not work

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,23 @@
+use std::collections::HashMap;
+use std::env;
+use std::ffi::OsString;
+use std::str::FromStr;
+
 use axum::{routing::get, Router};
 use sync_wrapper::SyncWrapper;
+use html2pdf::{run, CliOptions, Error};
+use structopt::StructOpt;
+
+fn html2pdf(url: String) -> Result<(), Error> {
+    let options= Vec::from([
+        "input".to_string(), "https://bgrande.de".to_string(),
+        "output".to_string(), "test.pdf".to_string(),
+   ]);
+    let opt = CliOptions::from_iter(options);
+
+    // Let's go
+    run(opt)
+}
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
