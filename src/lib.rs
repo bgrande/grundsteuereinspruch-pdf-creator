f845e71c2fa63636fee91f8f8b097422ff2a6713
Benedikt Grande <dev@bgrande.de>
2022-12-16 14:25:56 +0100

    feat(2pdf): working version
    missing html file for now

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,14 @@ use std::ops::Deref;
 use std::str::FromStr;
 
 use axum::{routing::get, Router};
+use axum::extract::Path;
 use sync_wrapper::SyncWrapper;
 use html2pdf::{run, CliOptions, Error};
 use structopt::StructOpt;
 
-fn html2pdf(url: String) -> Result<(), Error> {
+fn html2pdf(path: String) -> Result<(), Error> {
     let options= Vec::from([
-        "input".to_string(), url,
-        "output".to_string(), "test.pdf".to_string(),
+        "input", path.as_str()
    ]);
     let opt = CliOptions::from_iter(options);
 
@@ -23,18 +23,18 @@ fn html2pdf(url: String) -> Result<(), Error> {
 
 fn to_result(from_2pdf: Result<(), Error>) -> &'static str {
     return match from_2pdf.unwrap() {
-        () => "done",
-        _ => "sth. went wrong"
+        () => "done"
     }
 }
 
-async fn hello_world() -> &'static str {
-    to_result(html2pdf("https://bgrande.de".to_string()))
+async fn create_pdf(Path(params): Path<HashMap<String, String>>) -> &'static str {
+    let name = params.get("name");
+    to_result(html2pdf(format!("{}.html", name.unwrap())))
 }
 
 #[shuttle_service::main]
 async fn axum() -> shuttle_service::ShuttleAxum {
-    let router = Router::new().route("/hello", get(hello_world));
+    let router = Router::new().route("/create/:name", get(create_pdf));
     let sync_wrapper = SyncWrapper::new(router);
     Ok(sync_wrapper)
 }
