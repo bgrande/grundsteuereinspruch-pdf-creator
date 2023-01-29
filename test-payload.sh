ac570482714396a6ae4d5bee04085ff84f93d6b0
Benedikt Grande <dev@bgrande.de>
2023-01-29 16:19:35 +0100

    feat: payload testing
    open browser with target url immediately

--- test-payload.sh
+++ test-payload.sh
@@ -2,4 +2,6 @@
 curl -X POST \
   http://0.0.0.0:8000/html \
   -H 'Content-Type: application/json' \
-  -d @./payload-example.json
+  -d @./payload-example.json \
+  & \
+  browse "http://0.0.0.0:8000/formresult?subid=jZypz1&email=kontakt@bgrande.de&resp=2EReNj"
